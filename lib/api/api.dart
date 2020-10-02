10ac142bfee305233ddf2e032aae2b827f9b63f8
Shamith <abhi.shamith506@gmail.com>
2020-10-02 14:14:16 +0530

    Api: addTimeout, handle errors
    Signed-off-by: Shamith <abhi.shamith506@gmail.com>

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,13 +3,27 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:pcpartpicker/entities/guides.dart';
 
-final dioInstance = Dio(BaseOptions(
-  baseUrl: "https://pcpartpicker.herokuapp.com"
-));
+final dioInstance =
+    Dio(BaseOptions(baseUrl: "https://pcpartpicker.herokuapp.com"))
+      ..options.connectTimeout = 10000;
 
+Future getBuildGuides() async {
+  try {
+    final resp = await dioInstance.get('/guides');
+    return BuildGuides.fromJson(json.decode(resp.data));
+  } on DioError catch (Error) {
+    return Error;
+  }
+}
 
-Future<BuildGuides> getBuildGuides() async {
-  final resp = await dioInstance.get('/guides');
-
-  return BuildGuides.fromJson(json.decode(resp.data));
+Future getDetails({String path}) async {
+  try {
+    Dio dio = dioInstance;
+    dio.options.headers = {"path": path};
+    final resp = await dio.get('/gdetails');
+    print(resp.data);
+    return GuideDetails.fromJson(json.decode(resp.data));
+  } on DioError catch (Error) {
+    return Error;
+  }
 }
